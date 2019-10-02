9f3a3e01030810e99aad3600a5e177453ad1171c
Tomas Piga <tomp@theodo.co.uk>
2019-10-02 22:09:50 +0100

    [5] Turn background planet-like

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -54,13 +54,7 @@ class Home extends React.PureComponent<IProps, IState> {
     light.intensity = 0.7;
 
     // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 32, 8, scene);
 
     engine.runRenderLoop(() => {
       if (scene) {
