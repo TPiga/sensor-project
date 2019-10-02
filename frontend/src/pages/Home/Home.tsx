f61410f7c7f838ad1dec84ecac5cb12c45b9a280
Tomas Piga <tomp@theodo.co.uk>
2019-10-02 21:47:51 +0100

    [5] Show a simple Babylon view instead of graph

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,8 @@ import { getNumberOfMessages } from 'redux/Home';
 import { formatLineData, ILineData } from 'utilities/graph';
 import LineGraph from 'components/LineGraph';
 import { Button, Icon } from '@blueprintjs/core';
+import * as BABYLON from 'babylonjs';
+import BabylonScene, { SceneEventArgs } from 'components/BabylonJS/SceneComponent'; // import the component above linking to file we just created.
 
 interface IProps {
   getNumberOfMessages: typeof getNumberOfMessages.request;
@@ -36,13 +38,47 @@ class Home extends React.PureComponent<IProps, IState> {
     );
   }
 
+  onSceneMount = (e: SceneEventArgs) => {
+    const { canvas, scene, engine } = e;
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
+
+    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+
+    engine.runRenderLoop(() => {
+      if (scene) {
+        scene.render();
+      }
+    });
+  };
+
   render() {
     return (
       <HomeContainer>
         <Title>Welcome to my Challenge Project!</Title>
         <Body>
           <Container>
-            <LineGraph data={formatLineData(this.state.currentData)} />
+            <BabylonScene onSceneMount={this.onSceneMount} width={500} height={300} />
           </Container>
           <SideBar>
             <Button icon="refresh" intent="danger" text="Reset" />
